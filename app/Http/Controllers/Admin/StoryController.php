3402cfa56da6fbcc737484e32d027d16d6e9016e
ms-dev97 <mohammed.salem701@gmail.com>
2024-08-02 23:41:36 +0300

    create stories destroy action

--- app/Http/Controllers/Admin/StoryController.php
+++ app/Http/Controllers/Admin/StoryController.php
@@ -212,8 +212,9 @@ class StoryController extends Controller implements HasMiddleware
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Story $story)
     {
-        //
+        $story->delete();
+        return back()->with('success', 'تم الحذف بنجاح');
     }
 }
