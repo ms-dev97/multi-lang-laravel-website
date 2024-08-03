94695fdcd30f1349ce77e96b9b1a773b3224c1e0
ms-dev97 <mohammed.salem701@gmail.com>
2024-08-03 20:53:56 +0300

    statistics update method

--- app/Http/Controllers/Admin/StatisticsController.php
+++ app/Http/Controllers/Admin/StatisticsController.php
@@ -128,9 +128,53 @@ class StatisticsController extends Controller implements HasMiddleware
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, Statistic $statistic)
     {
-        //
+        $validated = $request->validate([
+            'name' => 'required|string',
+            'image' => ['nullable', 'image', 'max:2000'],
+            'order' => ['nullable', 'numeric', 'integer'],
+            'number' => ['required', 'numeric'],
+        ]);
+
+        $lang = $request->lang ?? env('APP_LOCALE');
+        $imagePath = $statistic->icon;
+        $newImagePath = null;
+
+        DB::beginTransaction();
+
+        try {
+            if ($request->has('image')) {
+                $newImagePath = $request->file('image')->store(self::Model_Directory);
+            }
+
+            $statistic->update([
+                'icon' => $newImagePath ?? $imagePath,
+                'order' => $validated['order'] ?? 0,
+                'number' => $validated['number'],
+                'status' => $request->has('status') ? true : false,
+                $lang => [
+                    'name' => $validated['name'],
+                ],
+            ]);
+
+            if (!is_null($newImagePath) && !is_null($imagePath)) {
+                // Make sure to delete old images before commiting and after saving new images if there are no errors
+                AdminHelpers::removeModelImage($imagePath);
+            }
+
+            DB::commit();
+
+            return redirect()->route('admin.statistics.index', ['lang' => $lang])->with('success', 'تم التعديل بنجاح');
+        } catch (\Throwable $th) {
+            if (!is_null($newImagePath)) {
+                AdminHelpers::removeModelImage($newImagePath);
+            }
+
+            DB::rollBack();
+
+            return back()->with('error', 'حدث خطأ غير متوقع! حاول مرة اخرى.');
+        }
     }
 
     /**
