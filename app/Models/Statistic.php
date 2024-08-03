12f0212a27037a9bf7ec4a2557bc5a9520a1d091
ms-dev97 <mohammed.salem701@gmail.com>
2024-08-03 19:00:55 +0300

    setup statistics model

--- app/Models/Statistic.php
+++ app/Models/Statistic.php
@@ -2,10 +2,27 @@
 
 namespace App\Models;
 
+use App\Helpers\AdminHelpers;
+use Astrotomic\Translatable\Contracts\Translatable as TranslatableContract;
+use Astrotomic\Translatable\Translatable;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Statistic extends Model
+class Statistic extends Model implements TranslatableContract
 {
-    use HasFactory;
+    use HasFactory, Translatable;
+
+    public $translatedAttributes = ['name'];
+    protected $fillable = ['number', 'icon', 'status'];
+
+    public function scopeActive(Builder $q) {
+        $q->where('status', 1);
+    }
+
+    protected static function booted(): void {
+        static::deleted(function (Statistic $statistic) {
+            AdminHelpers::removeModelImage($statistic->image);
+        });
+    }
 }
--- app/Models/StatisticTranslation.php
+++ app/Models/StatisticTranslation.php
@@ -8,4 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class StatisticTranslation extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['name'];
+    public $timestamps = false;
 }
