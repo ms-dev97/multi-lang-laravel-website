cca887f48ea9faf26efc994afbd2cdc968c86e88
ms-dev97 <mohammed.salem701@gmail.com>
2024-08-03 17:11:27 +0300

    setup video model

--- app/Models/Video.php
+++ app/Models/Video.php
@@ -2,10 +2,27 @@
 
 namespace App\Models;
 
+use App\Helpers\AdminHelpers;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Astrotomic\Translatable\Contracts\Translatable as TranslatableContract;
+use Astrotomic\Translatable\Translatable;
 
-class Video extends Model
+class Video extends Model implements TranslatableContract
 {
-    use HasFactory;
+    use HasFactory, Translatable;
+
+    public $translatedAttributes = ['title', 'excerpt', 'body'];
+    protected $fillable = ['slug', 'image', 'link', 'category_id', 'status', 'featured'];
+
+    public function scopeActive(Builder $q) {
+        $q->where('status', 1);
+    }
+
+    protected static function booted(): void {
+        static::deleted(function (Video $video) {
+            AdminHelpers::removeModelImage($video->image);
+        });
+    }
 }
