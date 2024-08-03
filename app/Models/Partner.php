e2405f6050be000ddc93686668a0d0ddb2f7d356
ms-dev97 <mohammed.salem701@gmail.com>
2024-08-03 14:15:19 +0300

    setup partner model

--- app/Models/Partner.php
+++ app/Models/Partner.php
@@ -2,10 +2,27 @@
 
 namespace App\Models;
 
+use App\Helpers\AdminHelpers;
+use Astrotomic\Translatable\Contracts\Translatable as TranslatableContract;
+use Astrotomic\Translatable\Translatable;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Partner extends Model
+class Partner extends Model implements TranslatableContract
 {
-    use HasFactory;
+    use HasFactory, Translatable;
+
+    public $translatedAttributes = ['name'];
+    protected $fillable = ['image', 'link', 'order', 'status'];
+
+    public function scopeActive(Builder $q) {
+        $q->where('status', 1);
+    }
+
+    protected static function booted(): void {
+        static::deleted(function (Partner $partner) {
+            AdminHelpers::removeModelImage($partner->image);
+        });
+    }
 }
