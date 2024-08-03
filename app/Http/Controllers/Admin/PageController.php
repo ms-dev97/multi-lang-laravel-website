2e6014268f31f344ea3ff11c6da3496e86f49f49
ms-dev97 <mohammed.salem701@gmail.com>
2024-08-03 23:28:47 +0300

    page destroy method

--- app/Http/Controllers/Admin/PageController.php
+++ app/Http/Controllers/Admin/PageController.php
@@ -201,8 +201,9 @@ class PageController extends Controller implements HasMiddleware
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Page $page)
     {
-        //
+        $page->delete();
+        return back()->with('success', 'تم الحذف بنجاح');
     }
 }
