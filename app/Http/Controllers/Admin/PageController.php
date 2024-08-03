b621667ee53c512e5b018bb8122f445796015f16
ms-dev97 <mohammed.salem701@gmail.com>
2024-08-03 22:08:50 +0300

    page index method

---
+++ app/Http/Controllers/Admin/PageController.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\Page;
+use Illuminate\Http\Request;
+use Illuminate\Routing\Controllers\HasMiddleware;
+use Illuminate\Routing\Controllers\Middleware;
+
+class PageController extends Controller implements HasMiddleware
+{
+    const Model_Directory = 'pages';
+
+    /**
+     * Get the middleware that should be assigned to the controller.
+     */
+    public static function middleware()
+    {
+        return [
+            new Middleware('can:browse-page', only: ['index']),
+            new Middleware('can:add-page', only: ['create', 'store']),
+            new Middleware('can:read-page', only: ['show']),
+            new Middleware('can:edit-page', only: ['edit', 'update']),
+            new Middleware('can:delete-page', only: ['destroy']),
+        ];
+    }
+
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $currentLang = request()->lang ?? env('APP_LOCALE');
+        $langs = config('translatable.locales');
+        $pages = Page::latest()
+            ->with('translations')
+            ->translatedIn($currentLang)->paginate(10)
+            ->withQueryString();
+
+        return view('admin.pages.index', compact('pages', 'currentLang', 'langs'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
---
+++ resources/views/admin/pages/create.blade.php
@@ -0,0 +1,105 @@
+@extends('admin.layout.app', [
+    'title' => 'إضافة سلايد | لوحة التحكم'
+])
+
+@section('main')
+    <div class="notifications">
+        @session('success')
+            @include('admin.partials.notification', [
+                'text' => session('success'),
+                'type' => 'success'    
+            ])
+        @endsession
+
+        @session('warning')
+            @include('admin.partials.notification', [
+                'text' => session('warning'),
+                'type' => 'warning'    
+            ])
+        @endsession
+
+        @session('error')
+            @include('admin.partials.notification', [
+                'text' => session('error'),
+                'type' => 'danger'    
+            ])
+        @endsession 
+    </div>
+
+    <div class="card">
+        <div class="card-header">
+            <div class="flex justify-content-between align-items-center g-1rem">
+                <div class="card-title">إضافة سلايد</div>
+                <a href="{{ route('admin.sliders.index') }}" class="ms-auto">عودة</a>
+                <button type="submit" class="btn btn-fill btn-primary" form="create">
+                    حفظ السلايد
+                </button>
+            </div>
+        </div>
+
+        @if ($errors->any())
+            <ul class="form-errors">
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        @endif
+
+        <div class="card-body">
+            <form action="{{ route('admin.sliders.store') }}" method="POST" id="create" class="main-form" enctype="multipart/form-data">
+                @csrf
+                <select class="lang-select form-control" name="lang" id="lang">
+                    @foreach ($langs as $lang)
+                        <option value="{{ $lang }}">
+                            {{ $lang }}
+                        </option>
+                    @endforeach
+                </select>
+
+                <div class="input-half">
+                    @include('admin.partials.text-input', [
+                        'type' => 'text',
+                        'name' => 'title',
+                        'id' => 'title',
+                        'label' => 'العنوان',
+                        'placeholder' => 'العنوان',
+                        'required' => true,
+                        'value' => old('title')
+                    ])
+
+                    <div class="form-group">
+                        <label for="order">الترتيب</label>
+                        <input type="number" name="order" id="order" class="form-control" min="0" value="0" placeholder="الترتيب">
+                    </div>
+                </div>
+
+                @include('admin.partials.image-input', [
+                    'id' => 'image',
+                    'name' => 'image',
+                    'label' => 'اختر صورة',
+                    'required' => false,
+                ])
+
+                <div class="form-group">
+                    <label for="slider_location">موقع السلايدر</label>
+                    <select name="slider_location" id="slider_location" class="form-control">
+                        <option value="1">الصفحة الرئيسية</option>
+                    </select>
+                </div>
+
+                <div class="form-group">
+                    @include('admin.partials.toggle', [
+                        'name' => 'status',
+                        'id' => 'status',
+                        'label' => 'الحالة',
+                        'checked' => true
+                    ])
+                </div>
+
+                <button type="submit" class="btn btn-fill btn-primary">
+                    حفظ السلايد
+                </button>
+            </form>
+        </div>
+    </div>
+@endsection
