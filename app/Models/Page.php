85895148cea72116d681b04e81517fd7b7ff0eb4
ms-dev97 <mohammed.salem701@gmail.com>
2024-08-03 22:02:22 +0300

    setup page model

--- app/Models/Page.php
+++ app/Models/Page.php
@@ -2,10 +2,22 @@
 
 namespace App\Models;
 
+use App\Helpers\AdminHelpers;
+use Astrotomic\Translatable\Contracts\Translatable as TranslatableContract;
+use Astrotomic\Translatable\Translatable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Page extends Model
+class Page extends Model implements TranslatableContract
 {
-    use HasFactory;
+    use HasFactory, Translatable;
+
+    public $translatedAttributes = ['name', 'excerpt', 'body'];
+    protected $fillable = ['slug', 'image', 'has_custom_view', 'view_name', 'status'];
+
+    protected static function booted(): void {
+        static::deleted(function (Page $page) {
+            AdminHelpers::removeModelImage($page->image);
+        });
+    }
 }
--- app/Models/PageTranslation.php
+++ app/Models/PageTranslation.php
@@ -8,4 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class PageTranslation extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['title', 'excerpt', 'body'];
+    public $timestamps = false;
 }
