f504d9a4d1ad8d36ce013e5cd4a275ec1912b0f9
ms-dev97 <mohammed.salem701@gmail.com>
2024-08-03 12:21:09 +0300

    create the store method

--- app/Http/Controllers/Admin/SliderController.php
+++ app/Http/Controllers/Admin/SliderController.php
@@ -2,11 +2,17 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Helpers\AdminHelpers;
 use App\Http\Controllers\Controller;
 use App\Models\Slider;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Controllers\HasMiddleware;
 use Illuminate\Routing\Controllers\Middleware;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
+use Intervention\Image\Encoders\AutoEncoder;
+use Intervention\Image\Laravel\Facades\Image;
 
 class SliderController extends Controller implements HasMiddleware
 {
@@ -57,7 +63,47 @@ class SliderController extends Controller implements HasMiddleware
      */
     public function store(Request $request)
     {
-        //
+        $validated = $request->validate([
+            'title' => 'required',
+            'image' => ['required', 'image', 'max:2000'],
+            'order' => ['nullable', 'numeric', 'integer'],
+            'slider_location' => ['nullable', 'integer'],
+        ]);
+
+        $lang = $request->lang ?? env('APP_LOCALE');
+        $imagePath = null;
+
+        DB::beginTransaction();
+
+        try {
+            if ($request->hasFile('image')) {
+                $imagePath = self::Model_Directory . '/' . Str::random(20) . '.' . $request->file('image')->getClientOriginalExtension();
+                $optimizedImg = Image::read($request->file('image'))->encode(new AutoEncoder(quality: 90));
+                Storage::disk('public')->put($imagePath, $optimizedImg);
+            }
+
+            Slider::create([
+                'image' => $imagePath,
+                'order' => $validated['order'],
+                'slider_location' => $validated['slider_location'] ?? 1,
+                'status' => $request->has('status') ? true : false,
+                $lang => [
+                    'title' => $validated['title']
+                ],
+            ]);
+
+            DB::commit();
+
+            return redirect()->route('admin.sliders.index', ['lang' => $lang])->with('success', 'تمت الاضافة بنجاح');
+        } catch (\Throwable $th) {
+            if (!is_null($imagePath)) {
+                AdminHelpers::removeModelImage($imagePath);
+            }
+
+            DB::rollBack();
+
+            return back()->with('error', 'حدث خطأ غير متوقع! حاول مرة اخرى.');
+        }
     }
 
     /**
