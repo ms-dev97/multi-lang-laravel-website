276f0d8190a862861cdeacbdee7db441e470dc6b
ms-dev97 <mohammed.salem701@gmail.com>
2024-08-03 11:30:44 +0300

    set up slider model

--- app/Models/Slider.php
+++ app/Models/Slider.php
@@ -2,10 +2,27 @@
 
 namespace App\Models;
 
+use App\Helpers\AdminHelpers;
+use Astrotomic\Translatable\Contracts\Translatable as TranslatableContract;
+use Astrotomic\Translatable\Translatable;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Slider extends Model
+class Slider extends Model implements TranslatableContract
 {
-    use HasFactory;
+    use HasFactory, Translatable;
+
+    public $translatedAttributes = ['title'];
+    protected $fillable = ['slider_location', 'image', 'order', 'status'];
+
+    public function scopeActive(Builder $q) {
+        $q->where('status', 1);
+    }
+
+    protected static function booted(): void {
+        static::deleted(function (Slider $slider) {
+            AdminHelpers::removeModelImage($slider->image);
+        });
+    }
 }
--- app/Models/SliderTranslation.php
+++ app/Models/SliderTranslation.php
@@ -8,4 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class SliderTranslation extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['title'];
+    public $timestamps = false;
 }
