c0d974ad9fe3a1ea0f120a98b563e8be190bec9c
ms-dev97 <mohammed.salem701@gmail.com>
2024-08-04 22:32:30 +0300

    google font

--- resources/views/admin/layout/app.blade.php
+++ resources/views/admin/layout/app.blade.php
@@ -3,6 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    {{-- Google font --}}
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Beiruti:wght@200..900&family=Cairo:wght@200..1000&display=swap" rel="stylesheet">
+
+    {{-- Main stylesheet --}}
     <link rel="stylesheet" href="{{ asset('assets/admin/css/main.css') }}" />
     @stack('styles')
     <title>{{ $title }}</title>
