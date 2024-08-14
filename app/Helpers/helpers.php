2fc6b348de098d37378826836764871346b61f70
ms-dev97 <mohammed.salem701@gmail.com>
2024-08-14 22:28:06 +0300

    model image and thumbnail helpers

--- app/Helpers/helpers.php
+++ app/Helpers/helpers.php
@@ -2,6 +2,8 @@
 
 use App\Models\Setting;
 use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Str;
+
 
 // Get website setting
 if (!function_exists('setting')) {
@@ -11,5 +13,22 @@ if (!function_exists('setting')) {
         });
 
         return is_null($key) ? $settings : $settings[$key];
+    }
+}
+
+// Get model image
+if (!function_exists('model_image')) {
+    function model_image($path) {
+        return asset('storage/' . $path);
+    }
+}
+
+// Get model thumbnail
+if (!function_exists('model_thumbnail')) {
+    function model_thumbnail($path) {
+        if (is_null($path)) return null;
+        $ext = pathinfo($path, PATHINFO_EXTENSION);
+        $path = Str::replaceLast('.'.$ext, '', $path) . '-sm.' . $ext;
+        return asset('storage/' . $path);
     }
 }
