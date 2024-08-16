0ef4d5e7e1edaf127c2897926d58809329e8ac0d
ms-dev97 <mohammed.salem701@gmail.com>
2024-08-16 14:24:31 +0300

    mail migration

---
+++ database/migrations/2024_08_10_201922_create_mails_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('mails', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->nullable();
+            $table->string('subject')->nullable();
+            $table->string('phone_number')->nullable();
+            $table->boolean('is_read')->default(false);
+            $table->unsignedTinyInteger('status')->default(0);
+            $table->text('message');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('mails');
+    }
+};
