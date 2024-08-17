1efb1c5b82cefbbeaa24e045ce2a12ec1370b1e7
ms-dev97 <mohammed.salem701@gmail.com>
2024-08-17 15:34:00 +0300

    cover card component

---
+++ resources/views/components/cards/cover-card.blade.php
@@ -0,0 +1,10 @@
+<a href="{{ $link }}" class="card cover-card position-relative">
+    <div class="cover">
+        <img src="{{ $cover }}" alt="{{ $title }}">
+    </div>
+
+    <div class="card-front position-absolute top-0 bottom-0 start-0 end-0 text-white d-flex flex-column align-items-center justify-content-center">
+        <img src="{{ $icon }}" alt="icon" class="icon">
+        <h4 class="card-title">{{ $title  }}</h4>
+    </div>
+</a>
