e2dd9b8c3b3953c583c9323ca78ca12465177326
ms-dev97 <mohammed.salem701@gmail.com>
2024-08-17 18:22:02 +0300

    translations

--- lang/ar/pages.php
+++ lang/ar/pages.php
@@ -4,4 +4,6 @@ return [
     'news' => 'الأخبار',
     'programs' => 'البرامج',
     'org_in_numbers' => 'المؤسسة في أرقام',
+    'partners' => 'الشركاء',
+    'our_partners' => 'شركاؤنا',
 ];
--- lang/en/pages.php
+++ lang/en/pages.php
@@ -4,4 +4,6 @@ return [
     'news' => 'News',
     'programs' => 'Programs',
     'org_in_numbers' => 'Organization in numbers',
+    'partners' => 'Partners',
+    'our_partners' => 'Our partners',
 ];
