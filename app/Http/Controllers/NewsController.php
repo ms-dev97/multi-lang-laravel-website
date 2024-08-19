258ebb62310ff60af85b3b45c511633731573c53
ms-dev97 <mohammed.salem701@gmail.com>
2024-08-19 22:43:41 +0300

    news controller

--- app/Http/Controllers/NewsController.php
+++ app/Http/Controllers/NewsController.php
@@ -8,11 +8,21 @@ use Illuminate\Http\Request;
 class NewsController extends Controller
 {
     public function index() {
-
+        $news = News::active()->latest()->withTranslation()->translatedIn(app()->getLocale())->paginate(9);
+        return view('public.news.index', compact('news'));
     }
 
     public function show(News $news) {
+        if (!$news->isActive()) abort(404);
+
         $item = $news;
-        return view('public.news.show', compact('item'));
+        $related = News::where('id', '!=', $news->id)
+            ->active()
+            ->latest()
+            ->withTranslation()
+            ->translatedIn(app()->getLocale())
+            ->take(3)->get();
+
+        return view('public.news.show', compact('item', 'related'));
     }
 }
