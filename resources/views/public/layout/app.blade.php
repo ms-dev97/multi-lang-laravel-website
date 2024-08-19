1018ad704a1712943c9cf6f0d305d31132706c10
ms-dev97 <mohammed.salem701@gmail.com>
2024-08-19 23:17:29 +0300

    add google font

--- resources/views/public/layout/app.blade.php
+++ resources/views/public/layout/app.blade.php
@@ -34,6 +34,11 @@
         <link rel="alternate" href="{{ LaravelLocalization::getLocalizedURL($localeCode, url()->current()) }}" hreflang="{{ $localeCode }}">
     @endforeach
 
+    {{-- Google font --}}
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Beiruti:wght@200..900&family=Cairo:wght@200..1000&display=swap" rel="stylesheet">
+
     {{-- Styles --}}
     @vite(['resources/sass/app.scss'])
 
