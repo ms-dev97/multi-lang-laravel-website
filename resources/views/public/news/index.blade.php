08410f2bb989c743463ea2d79ab6d195b35a4805
ms-dev97 <mohammed.salem701@gmail.com>
2024-08-19 22:42:03 +0300

    news index page

---
+++ resources/views/public/news/index.blade.php
@@ -0,0 +1,37 @@
+@php
+    $title = __('pages.news');
+    $description = __('pages.news_description');
+    $image = getImgFromPath(setting('header_logo'));
+@endphp
+
+@extends('public.layout.app', [
+    'title' => $title . ' | ' . __('general.website_name'),
+    'description' => $description,
+    'metaImage' => $image
+])
+
+@section('content')
+    <main class="news-page">
+        <div class="container">
+            <h1 class="page-title">{{ $title }}</h1>
+
+            <section class="row row-gap-5">
+                @foreach ($news as $item)
+                    <article class="col-md-4">
+                        @php
+                            $newsText = $item->translate()->excerpt ?? Str::limit(strip_tags($item->translate()->body), 300)
+                        @endphp
+
+                        <x-cards.basic-card
+                            :title="$item->translate()->title"
+                            :text="Str::limit($newsText, 300)"
+                            :img="getImgFromPath($item->image)"
+                            :link="route('news.show', $item)"
+                            :date="$item->created_at"
+                        />
+                    </article>
+                @endforeach
+            </section>
+        </div>
+    </main>
+@endsection
