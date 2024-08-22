daa1ecc5cc22901ab784771b6b128ba4778d843d
ms-dev97 <mohammed.salem701@gmail.com>
2024-08-22 23:55:11 +0300

    seed settings

--- database/seeders/SettingSeeder.php
+++ database/seeders/SettingSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Setting;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,15 @@ class SettingSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Setting::insert([
+            ['key' => 'facebook_id', 'display_name' => 'معرف صفحة الفيسبوك', 'type' => 'text', 'group' => 'info'],
+            ['key' => 'facebook_link', 'display_name' => 'رابط صفحة الفيسبوك', 'type' => 'text', 'group' => 'info'],
+            ['key' => 'twitter_id', 'display_name' => 'معرف صفحة تويتر', 'type' => 'text', 'group' => 'info'],
+            ['key' => 'twitter_link', 'display_name' => 'رابط صفحة تويتر', 'type' => 'text', 'group' => 'info'],
+            ['key' => 'linkedin_link', 'display_name' => 'رابط صفحة لنكد ان', 'type' => 'text', 'group' => 'info'],
+            ['key' => 'instagram_link', 'display_name' => 'رابط انستجرام', 'type' => 'text', 'group' => 'info'],
+            ['key' => 'header_logo', 'display_name' => 'شعار الموقع (الهيدر)', 'type' => 'image', 'group' => 'backgrounds'],
+            ['key' => 'footer_logo', 'display_name' => 'شعار الموقع (الفوتر)', 'type' => 'image', 'group' => 'backgrounds'],
+        ]);
     }
 }
