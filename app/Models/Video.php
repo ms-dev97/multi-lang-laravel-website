9a723fa5d66f97c52e97915a9293e63f641f6b3d
ms-dev97 <mohammed.salem701@gmail.com>
2024-08-30 00:16:39 +0300

    video iframe and image

--- app/Models/Video.php
+++ app/Models/Video.php
@@ -17,6 +17,23 @@ class Video extends Model implements TranslatableContract
     public $translatedAttributes = ['title', 'excerpt', 'body'];
     protected $fillable = ['slug', 'image', 'link', 'category_id', 'status', 'featured'];
 
+    public function vidID() {
+        preg_match("#(?<=v=)[a-zA-Z0-9-]+(?=&)|(?<=v\/)[^&\n]+(?=\?)|(?<=v=)[^&\n]+|(?<=youtu.be/)[^&\n]+#", $this->link, $matches);
+        return end($matches);
+    }
+
+    public function vidImage() {
+        return 'https://i.ytimg.com/vi/' . $this->vidID() . '/hqdefault.jpg';
+    }
+
+    public function vidFrame() {
+        return '<iframe width="100%" height="400"
+                src="https://www.youtube.com/embed/' . $this->vidID() . '"
+                title="YouTube video player" frameborder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowfullscreen></iframe>';
+    }
+
     public function scopeActive(Builder $q) {
         $q->where('status', 1);
     }
