b36bbf1cd78e3dc8b93dbc140eac53ea57d87905
ms-dev97 <mohammed.salem701@gmail.com>
2024-08-30 00:16:17 +0300

    videos show page

---
+++ resources/views/public/videos/show.blade.php
@@ -0,0 +1,59 @@
+@php
+    $title = $itemTrans->title;
+    $excerpt = $itemTrans->excerpt;
+    $content = $itemTrans->body;
+    $description = $excerpt ??  Str::limit(html_entity_decode(strip_tags($content)), 300);
+    $image = $item->image ?? $item->vidImage();
+@endphp
+
+@extends('public.layout.app', [
+    'title' => $title . ' | ' . __('general.website_name'),
+    'description' => $description,
+    'metaImage' => $image
+])
+
+@section('content')
+    <div class="videos-page-single">
+        <main>
+            <div class="container my-5">
+                {{-- Main title --}}
+                <h1 class="page-title">{{ $title }}</h1>
+                {{-- Date --}}
+                <x-date :date="$item->created_at" />
+                {{-- Video frame --}}
+                <div class="my-4">
+                    {!! $item->vidFrame() !!}
+                </div>
+                {{-- Body content --}}
+                <div class="body-container">
+                    {!! $content !!}
+                </div>
+            </div>
+        </main>
+
+        {{-- Related items --}}
+        @if ($related->count() > 0)
+            <section class="related-items my-5">
+                <div class="container">
+                    <h3 class="related-items">{{ __('pages.more_videos') }}</h3>
+                    <div class="row row-gap-5">
+                        @foreach ($related as $item)
+                            <article class="col-md-4">
+                                @php
+                                    $itemText = $item->translate()->excerpt ?? html_entity_decode(strip_tags($item->translate()->body))
+                                @endphp
+                                <x-cards.basic-card
+                                    :title="$item->translate()->title"
+                                    :text="Str::limit($itemText, 300)"
+                                    :img="getImgFromPath($item->image)"
+                                    :link="route('videos.show', $item)"
+                                    :date="$item->created_at"
+                                />
+                            </article>
+                        @endforeach
+                    </div>
+                </div>
+            </section>
+        @endif
+    </div>
+@endsection
