e5fca65341646f00397c8d176f2179930bc4efa5
ms-dev97 <mohammed.salem701@gmail.com>
2024-09-18 23:16:58 +0300

    sanitize user input

--- app/Http/Controllers/MailController.php
+++ app/Http/Controllers/MailController.php
@@ -15,7 +15,25 @@ class MailController extends Controller
             'message' => 'string|required',
         ]);
 
+        $request['name'] = $this->sanitizeUserInput($request->name);
+        $request['subject'] = $this->sanitizeUserInput($request->subject);
+        $request['phone_number'] = $this->sanitizeUserInput($request->phone_number);
+        $request['email'] = $this->sanitizeUserInput($request->email);
+        $request['message'] = $this->sanitizeUserInput($request->message);
+
         Mail::create($request->all());
         return redirect()->route('pages.show', 'contact-us')->with('success', __('app.message_sent'));
+    }
+
+    /**
+     * Remove HTML tags, convert special chars to HTML entities and convert nl to br
+     * @param string
+     * @return string
+     */
+    private function sanitizeUserInput($input) {
+        $input = strip_tags($input);
+        $input = htmlentities($input, ENT_QUOTES, 'utf-8');
+        $input = nl2br($input);
+        return $input;
     }
 }
